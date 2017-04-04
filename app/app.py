321737841e6035d7962a7fb066a24d3d36ecbf0c
Gul Ahmed <gulaftab@gmail.com>
2017-04-04 11:44:38 +0500

    added twitter bootstrap

--- app/app.py
+++ app/app.py
@@ -1,19 +1,28 @@
 """
 A basic starter app  with the Flask framework and PyMongo
 """
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello world"
+    return render_template("home.html")
+
+@app.route('/about')
+def aboutUs():
+    return render_template("aboutus.html")
+
+@app.route('/contact')
+def contactUs():
+    return render_template("contactus.html")
+
 
 
 @app.route('/hello/<name>')
 def saymyname(name):
-    return "Hello %s" % name
+        return render_template("index.html",name=name)
 
 
 
