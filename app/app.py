506502fca9158800c7b5516f622663a2308113c0
Gul Ahmed <gulaftab@gmail.com>
2017-04-11 13:43:01 +0500

    use of MongoDB is demonstrated

--- app/app.py
+++ app/app.py
@@ -1,10 +1,23 @@
 """
 A basic starter app  with the Flask framework and PyMongo
 """
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
+
+app.config['MONGO_HOST'] = 'localhost'
+app.config['MONGO_PORT']='27017'
+app.config['MONGO_DBNAME']="lsdda"
+
+# connect to MongoDB with the defaults
+#mongo    = PyMongo(app)
+
+mongo = PyMongo(app,config_prefix='MONGO')
+
+
+
 
 @app.route('/')
 def home():
@@ -14,11 +27,15 @@ def home():
 def aboutUs():
     return render_template("aboutus.html")
 
-@app.route('/contact', methods=["GET","POST"])
-def contactUs():
-    if(request.method == "GET"):
-        return render_template("contactus.html")
-    elif (request.method == "POST"):
+
+@app.route('/contact')
+def getcontactUs():
+    return render_template("contactus.html")
+
+
+@app.route('/contact', methods=["POST"])
+def postcontactUs():
+
         name = request.form['inputName']
         email = request.form['inputEmail']
         comments = request.form['textAreaComments']
@@ -28,16 +45,23 @@ def contactUs():
          "email":email,
          "comments": comments
         }
-        return jsonify(result=data);
+
+        try:
+            mongo.db.contactUs.insert_one(data)
+            return redirect(url_for('all_comments'))
+        except err:
+            return "Inesrt Failed: Something wentwrong"
+        #return jsonify(result=data);
 
         #return render_template("contactResponse.html",result=data)
 
 
+@app.route('/comments')
+def all_comments():
+    comments = mongo.db.contactUs.find()
+    return render_template("comments.html", comments=comments)
 
 
-@app.route('/hello/<name>')
-def saymyname(name):
-        return render_template("index.html",name=name)
 
 
 
